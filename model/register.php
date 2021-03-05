5665b69236fa09881e6427048c864a75e9f8e007
Shawn Potter <spotter@mail.greenriver.edu>
2021-03-05 01:38:44 -0500

    added inital register.php file

--- model/register.php
+++ model/register.php
@@ -1,1 +1,19 @@
 <?php
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//Require files
+require $_SERVER['DOCUMENT_ROOT'].'/../ht-db-config.php';
+
+//Define the query
+$sql = "";
+
+//Prepare the statement
+$statement = $dbh->prepare($sql);
+
+//Bind the parameters
+$statement->bindParam('', $_POST[''], PDO::PARAM_STR);
+
+//Execute
+$statement->execute();
