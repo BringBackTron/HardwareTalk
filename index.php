f9608b423885cad00e58edaf961845412695df52
Shawn Potter <spotter@mail.greenriver.edu>
2021-03-07 20:52:29 -0500

    added class instantiation to index, changed login and register routes to GET|POST

--- index.php
+++ index.php
@@ -1,43 +1,67 @@
 <?php
-  /*
-   *
-   */
-  
-  //The Controller
-  
-  //turn on error reporting
-  ini_set('display_errors', 1);
-  error_reporting(E_ALL);
-  
-  //start a session
-  session_start();
-  
-  //require autoload file
-  require_once("vendor/autoload.php");
-  
-  //create an instance of the base class
-  $f3 = Base::instance();
-  
-  //set fat-free debugging
-  $f3->set('DEBUG', 3);
-  
-  //Define a default route (home page)
-  $f3->route('GET /', function(){
-    // render home.html
-    $view = new Template();
-    echo $view->render('views/home.html');
-  });
+/*
+ *
+ */
 
-    $f3->route('GET /login', function(){
-    // render login.html
-    $view = new Template();
-    echo $view->render('views/login.html');
-    });
+//The Controller
 
-$f3->route('GET /register', function(){
-    // render register.html
-    $view = new Template();
-    echo $view->render('views/register.html');
+//turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+//start a session
+session_start();
+
+//require autoload file
+require $_SERVER['DOCUMENT_ROOT'].'/../ht-db-config.php';
+require_once("vendor/autoload.php");
+
+
+//create an instance of the base class
+$f3 = Base::instance();
+
+//instanitate classes
+$register = new Register($dbh, $f3);
+$login = new Login($dbh, $f3);
+$validator = new Validator();
+
+
+//set fat-free debugging
+$f3->set('DEBUG', 3);
+
+//Define a default route (home page)
+$f3->route('GET /', function(){
+  // render home.html
+  $view = new Template();
+  echo $view->render('views/home.html');
+});
+
+$f3->route('GET|POST /login', function(){
+  global $login;
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $login->logInUser();
+  }
+
+  // render login.html
+  $view = new Template();
+  echo $view->render('views/login.html');
+});
+
+$f3->route('GET|POST /register', function($f3, $dbh){
+  global $register;
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $register->registerUser();
+}
+  echo "<pre>";
+  echo "POST ARRAY:";
+  echo print_r($_POST,true);
+  echo "<br> SESSION ARRAY:";
+  echo print_r($_SESSION,true);
+  echo "</pre>";
+  // render register.html
+  $view = new Template();
+  echo $view->render('views/register.html');
+  
 });
 
 
