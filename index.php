2010113d604bdf54cc124961c983791940e27500
Shawn Potter <spotter@mail.greenriver.edu>
2021-03-14 00:49:07 -0500

    finished merging

--- index.php
+++ index.php
@@ -32,70 +32,73 @@ $f3->set('DEBUG', 3);
 
 //Define a default route (home page)
 $f3->route('GET /', function($f3){
-    $view = new Template();
-    if($_SESSION['loggedin'] == true) {
-        echo $view->render('views/feed.html');
-    } else {
-        echo $view->render('views/home.html');
-    }
+  $view = new Template();
+  if($_SESSION['loggedin'] == true) {
+    echo $view->render('views/feed.html');
+  } else {
+    echo $view->render('views/home.html');
+  }
 
 });
 
 $f3->route('GET|POST /login', function($f3){
-    global $login;
-    if($_SESSION['loggedin'] == true) {
-        $f3->reroute('/');
-    }
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $login->logInUser();
-    }
-    if(!empty($f3->get('success')) && empty($f3->get('errors'))){
-        $f3->reroute('/');
-    }
+  global $login;
+  if($_SESSION['loggedin'] == true) {
+    $f3->reroute('/');
+  }
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $login->logInUser();
+  }
+  if(!empty($f3->get('success')) && empty($f3->get('errors'))){
+    $f3->reroute('/');
+  }
 
-    // render login.html
-    $view = new Template();
-    echo $view->render('views/login.html');
+  // render login.html
+  $view = new Template();
+  echo $view->render('views/login.html');
 });
 
 $f3->route('GET|POST /register', function($f3, $dbh){
-    global $register;
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-        $register->registerUser();
-        if(empty($f3->get('errors'))){
-            $f3->reroute('/');
-        }
+  global $register;
+  if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $register->registerUser();
+    if(empty($f3->get('errors'))){
+      $f3->reroute('/');
     }
+  }
 
-    /*echo "<pre>";
-    echo "POST ARRAY:";
-    echo print_r($_POST,true);
-    echo "<br> SESSION ARRAY:";
-    echo print_r($_SESSION,true);
-    echo "</pre>";*/
+  /*echo "<pre>";
+  echo "POST ARRAY:";
+  echo print_r($_POST,true);
+  echo "<br> SESSION ARRAY:";
+  echo print_r($_SESSION,true);
+  echo "</pre>";*/
 
-    // render register.html
-    $view = new Template();
-    echo $view->render('views/register.html');
+  // render register.html
+  $view = new Template();
+  echo $view->render('views/register.html');
 
 });
 
 $f3->route('GET /gaming', function(){
 //    echo var_dump($_SESSION);
-    // render home.html
-    $view = new Template();
-    echo $view->render('views/communities/gaming.html');
+  // render home.html
+  $view = new Template();
+  echo $view->render('views/communities/gaming.html');
 });
 
-$f3->route('GET /community/9', function(){
+$f3->route('GET /community/9', function($f3){
 
-    global $community;
-    //echo var_dump($_SESSION);
-    // render home.html
-    $community->viewPosts();
+  global $community;
+  //echo var_dump($_SESSION);
+  // render home.html
+  $community->viewPosts();
+  /*echo "<pre>";
+  echo print_r($f3->get("posts"));
+  echo "</pre>";*/
 
-    $view = new Template();
-    echo $view->render('views/communities/diy.html');
+  $view = new Template();
+  echo $view->render('views/communities/diy.html');
 
 });
 
