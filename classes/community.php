9a0d8a159785e297fdf05fd597d3152b97c43547
Shawn Potter <spotter@mail.greenriver.edu>
2021-03-14 00:10:58 -0500

    added display of posts from database and updated viewPosts function in community class

--- classes/community.php
+++ classes/community.php
@@ -10,28 +10,39 @@ class Community
   }
   function viewPosts()
   {
-    $sql = "SELECT * FROM posts WHERE community_id = :community_id";
+    $sql = "SELECT * FROM posts WHERE community_id = :community_id ORDER BY post_creation_date DESC ";
     if($statement = $this->_dbh->prepare($sql)){
-      echo "statement prepared"; //debug
+      //echo "statement prepared"; //debug
+      $url = $_SERVER['REQUEST_URI'];
+      $url = explode('/', $url);
+      $community_id = array_pop($url);
+      $statement->bindParam(":community_id", $community_id, PDO::PARAM_STR);
     } else {
-      echo "statement failed to prepare"; //debug
+      //echo "statement failed to prepare"; //debug
     }
-    var_dump($_SERVER['REQUEST_URI']);
-    $url = $_SERVER['REQUEST_URI'];
-    $url = explode('/', $url);
-    $community_id = array_pop($url);
-    $statement->bindParam(":community_id", $community_id, PDO::PARAM_STR);
+    //var_dump($_SERVER['REQUEST_URI']); // debug
+    if($statement->execute()){
+      //echo "statement executed";
+      $results = $statement->fetchAll();
+      $now = new DateTime("now");
+      foreach ($results as $key => $item){
+        $time = $results[$key]['post_creation_date'];
+        $time = new DateTime($time, new DateTimeZone('EST'));
+        // var_dump($time); // debug
+        $time = $time->diff($now);
+        $results[$key]['post_creation_date'] = $time->format("%h");
+      }
+      $this->_f3->set("posts", $results);
+    }
+    else {
+      //echo "statement failed";
+    }
 
-    if($statement->execute())
-      echo "statement executed";
-    else
-      echo "statement failed";
 
-    $results = $statement->fetchAll();
-    $this->_f3->set("posts", $results);
-    echo "<pre>";
-    echo print_r($this->_f3->get("posts"));
-    echo "</pre>";
+
+    /*echo "\nPDOStatement::errorInfo():\n";
+    $arr = $statement->errorInfo();
+    print_r($arr);*/
 
   }
 }
