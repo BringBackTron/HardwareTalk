990c81e74b8983eb9d237e1a2b8e348701fa10d0
Shawn Potter <spotter@mail.greenriver.edu>
2021-03-16 08:04:49 -0400

    updated f3 variables and cleaned up debug statements. changed PDO method of getting variable for binding.

--- classes/community.php
+++ classes/community.php
@@ -8,41 +8,63 @@ class Community
     $this->_dbh =  $dbh;
     $this->_f3 = $f3;
   }
-  function viewPosts()
+  function viewPosts($id)
   {
     $sql = "SELECT * FROM posts WHERE community_id = :community_id ORDER BY post_creation_date DESC ";
     if($statement = $this->_dbh->prepare($sql)){
-      //echo "statement prepared"; //debug
-      $url = $_SERVER['REQUEST_URI'];
-      $url = explode('/', $url);
-      $community_id = array_pop($url);
-      $statement->bindParam(":community_id", $community_id, PDO::PARAM_STR);
-    } else {
-      //echo "statement failed to prepare"; //debug
+      
+      /* Debug */
+      // echo "statement prepared";
+      
+      /* old method of getting community id */
+      // $url = $_SERVER['REQUEST_URI'];
+      // $url = explode('/', $url);
+      // $community_id = array_pop($url);
+      
+      $statement->bindParam(":community_id", $id, PDO::PARAM_STR);
+      
     }
-    //var_dump($_SERVER['REQUEST_URI']); // debug
+    /* Currently a Debug statement, change to something else like an error page */
+    /*
+    else {
+      echo "statement failed to prepare";
+    }
+    */
+    
+    
     if($statement->execute()){
-      //echo "statement executed";
+      
+      /* Debug */
+      // echo "statement executed";
+      
       $results = $statement->fetchAll();
       $now = new DateTime("now");
       foreach ($results as $key => $item){
         $time = $results[$key]['post_creation_date'];
         $time = new DateTime($time, new DateTimeZone('EST'));
-        // var_dump($time); // debug
+  
+        /* Debug */
+        // var_dump($time);
+        
         $time = $time->diff($now);
         $results[$key]['post_creation_date'] = $time->format("%h");
       }
       $this->_f3->set("posts", $results);
     }
+    /* Debug */
+    /*
     else {
       //echo "statement failed";
     }
-
-
-
-    /*echo "\nPDOStatement::errorInfo():\n";
+    */
+  
+  
+    /* Debug */
+    /*
+    echo "\nPDOStatement::errorInfo():\n";
     $arr = $statement->errorInfo();
-    print_r($arr);*/
+    print_r($arr);
+    */
 
   }
 }
