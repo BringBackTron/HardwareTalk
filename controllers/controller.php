6ae11a7f6b8ed3f011bfb0a14dd8a34948de6ea3
Shawn Potter <spotter@mail.greenriver.edu>
2021-03-19 21:43:09 -0400

    added TODO, commented out debug statements, switched loggedin check to differen't position in function

--- controllers/controller.php
+++ controllers/controller.php
@@ -17,16 +17,19 @@ class Controller
   {
     global $data;
 
-    $view = new Template();
-    if($_SESSION['loggedin'] == true) {
-      echo $view->render('views/feed.html');
-    } else {
-      echo $view->render('views/home.html');
-    }
-
     for($i = 1; $i < 10; $i++) {
       $data->updateLastPosted($i);
     }
+
+    $view = new Template();
+    echo $view->render('views/home.html');
+    /*if($_SESSION['loggedin'] == true) {
+      echo $view->render('views/feed.html');
+    } else {
+      echo $view->render('views/home.html');
+    }*/
+
+
   }
 
   function login()
@@ -115,6 +118,39 @@ class Controller
 
     $view = new Template();
     echo $view->render('views/comments.html');
+  }
+
+  // TODO: Sanitize
+  function submit($community_id)
+  {
+    echo "submit function reached";
+    global $community;
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+      echo "request method reached";
+      $subject = $_POST['postSubject'];
+      if(!empty($_POST['postText'])) {
+        $text = $_POST['postText'];
+      } else {
+        $text = "";
+      }
+      if($_POST['postMedia']) {
+        $media = $_POST['postMedia'];
+      } else {
+        $media = "";
+      }
+      /* Debug */
+      // echo $media;
+
+      if(!$_SESSION['loggedin']) {
+        $this->_f3->reroute("login");
+      }
+
+      $community->submitPost($community_id, $subject, $text, $media);
+    }
+
+      $view = new Template();
+      echo $view->render('views/submit.html');
   }
 
 
